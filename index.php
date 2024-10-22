8ec5ece99c1cce5fd0000224d2c949e77649bc36
root <mtrhoades0727@gmail.com>
2024-10-22 12:32:13 -1000

    adding html form

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="functions.php" method="get">
+        <input type="text" name="id" placeholder="num01" placeholder="Number 1">
+        <select>
+            <label>Choose Operation!</label>
+            <option value="add">Add</option>
+            <option value="sub">Subtract</option>
+        </select>
+        <input type="text" name="num02" placeholder="Number 2">
+        <button type="submit">Calculate!</button>
+    </form>
 </body>
 </html>
