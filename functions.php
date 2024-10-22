58648490e10f6fe9ed8a1651262d32b765fbada1
root <mtrhoades0727@gmail.com>
2024-10-22 13:35:20 -1000

    adding functions

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function myCalculator($num01, $oper, $num02) {
+    $sum;
+    switch ($oper) {
+        case "add":
+            $sum = $num01 + $num02;
+            break;
+        case "sub":
+            $sum = $num01 - $num02;
+            break;
+        default:
+            $sum = "There was an error!";
+            break;
+    }
+    return $sum;
+}
+
+?>
